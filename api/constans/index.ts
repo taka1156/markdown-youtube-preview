7cd249caf9c61ad8e3046bce966f5f5d80079434
taka <47517002+taka1156@users.noreply.github.com>
2023-09-18 05:25:37 +0000

    初期値の修正、リファクタリング

--- api/constans/index.ts
+++ api/constans/index.ts
@@ -1,4 +1,4 @@
-const playIcon = (colorCode = "000000", size="100") => {
+const playIcon = (colorCode = "ff0000", size="100") => {
    return `https://images.microcms-assets.io/assets/ed840bb775fe400dae402a2340344206/b9aa113062834684aece12f3672b3581/play-black.png?w=${size}&h=${size}&duotone=${colorCode},FFFFFF`
 }
 
--- api/routes/v1/index.ts
+++ api/routes/v1/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import { playIcon } from "../../constans"
-import sharp from "sharp";
+import sharp, { Metadata } from "sharp";
 import axios from "axios";
 const router = express.Router();
 import 'dotenv/config'
+import { arrayBuffer } from "stream/consumers";
 
 // 簡易的なサーバーの生死確認
 router.get('/', (req, res) => {
@@ -16,62 +17,71 @@ const HEADERS = {
     'cache-control': 'max-age=0, no-cache, no-store, must-revalidate',
 };
 
-const generate = async (videoId: string, colorCode: string, size: string): Promise<Buffer> => {
 
-    const backgroundImageUrl = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-    const playImageUrl = playIcon(colorCode, size);
+const fetchImage = async (url: string): Promise<Buffer> => {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    return response.data;
+}
 
-    const bgImage = await axios.get(backgroundImageUrl, { responseType: 'arraybuffer' }).then(response => response.data);
-    const playImage = await axios.get(playImageUrl, { responseType: 'arraybuffer' }).then(response => response.data);
-
-    const backgroundImage = sharp(bgImage);
-    const overlayImage = sharp(playImage);
-
-    const backgroundMetadata = await backgroundImage.metadata();
-    const overlayMetadata = await overlayImage.metadata();
-
-    let offsetX: number = 0;
-    let offsetY: number = 0;
-
-    if (backgroundMetadata.width != null && overlayMetadata.width != null && backgroundMetadata.height != null && overlayMetadata.height != null) {
-        offsetX = (backgroundMetadata.width - overlayMetadata.width) / 2;
-        offsetY = (backgroundMetadata.height - overlayMetadata.height) / 2;
+const calOffset = (backgroundMetadata: Metadata, overlayMetadata: Metadata) => {
+    if (backgroundMetadata.width && overlayMetadata.width && backgroundMetadata.height && overlayMetadata.height) {
+        return {
+            offsetX: (backgroundMetadata.width - overlayMetadata.width) / 2,
+            offsetY: (backgroundMetadata.height - overlayMetadata.height) / 2
+        };
+    } else {
+        return {
+            offsetX: 0,
+            offsetY: 0
+        };
     }
+}
 
-    return backgroundImage
-        .composite([{
-            input: playImage,
-            left: offsetX,
-            top: offsetY
-        }])
-        .toBuffer();
+const generateOverlayImage = async (videoId: string, colorCode: string, size: string): Promise<Buffer> => {
+
+    try {
+        const backgroundImageUrl = `https://img.youtube.com/vi/${videoId}/0.jpg`;
+        const playImageUrl = playIcon(colorCode, size);
+
+        const [bgImage, playImage] = await Promise.all([
+            fetchImage(backgroundImageUrl),
+            fetchImage(playImageUrl)
+        ]);
+
+        const backgroundImage = sharp(bgImage);
+        const overlayImage = sharp(playImage);
+
+        const backgroundMetadata = await backgroundImage.metadata();
+        const overlayMetadata = await overlayImage.metadata();
+
+        const { offsetX, offsetY } = calOffset(backgroundMetadata, overlayMetadata);
+
+
+        return backgroundImage
+            .composite([{
+                input: playImage,
+                left: offsetX,
+                top: offsetY
+            }])
+            .toBuffer();
+    } catch (e: unknown) {
+        console.error(e);
+        throw new Error("Faild to generate image.");
+    }
 }
 
 router.get('/yt/:videoId', asyncHandler(async (req, res) => {
-    const { videoId } = req.params;
-    let colorCode = (req.query.color || "") as string;
-    let size  = (req.query.size || "") as string;
+    try {
+        const { videoId } = req.params;
+        const colorCode = (req.query.color || req.query.c || "ff0000") as string;
+        const size = (req.query.size || req.query.s || "100") as string;
 
-    if (colorCode === "" && req.query?.c != null && req.query?.c !== "") {
-        colorCode = req.query?.c as string;
-    }
-
-    if (size === "" && req.query?.s != null && req.query?.s !== "") {
-        size = req.query?.s as string;
-    }
-
-    const data = await generate(videoId, colorCode, size).
-        then((buffer) => {
-            return buffer;
-        }).
-        catch(e => {
-            console.log(e);
-            return null;
-        });
-
-    res.set(HEADERS);
-    res.send(data);
-
+        const data = await generateOverlayImage(videoId, colorCode, size);
+        res.set(HEADERS);
+        res.send(data);
+    } catch (e: unknown) {
+        res.status(500).send(e);
+    };
 }));
 
 export {
